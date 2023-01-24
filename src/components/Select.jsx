6e7268313f4d6519092a40361970ecd2541c628b
LucasAndria <lucasandria01@gmail.com>
2023-01-24 21:44:55 +0300

    implementing the single select option

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,18 +1,61 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "../select.module.css";
 
 export function Select({ value, onChange, options }) {
   const containerRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [highlightedIndex, setHighlightedIndex] = useState(0);
+
+  function clearOptions() {
+    onChange(undefined);
+  }
+
+  function selectOption(option) {
+    if (option !== value) onChange(option);
+  }
+
+  function isOptionSelected(option) {
+    return option === value;
+  }
+
+  useEffect(() => {
+    if (isOpen) setHighlightedIndex(0);
+  }, [isOpen]);
 
   return (
-    <div tabIndex={0} className={styles.container}>
-      <span className={styles.value}>{value.label}</span>
-      <button className={styles["clear-btn"]}>&times;</button>
+    <div
+      ref={containerRef}
+      onClick={() => setIsOpen((prev) => !prev)}
+      onBlur={() => setIsOpen(false)} // Excécute a fonction when somewhere else is clicked
+      tabIndex={0} // To enable the unFocus style when somewhere else is clicked
+      className={styles.container}
+    >
+      <span className={styles.value}>{value?.label}</span>
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+          clearOptions();
+        }}
+        className={styles["clear-btn"]}
+      >
+        &times;
+      </button>
       <div className={styles.divider}></div>
       <div className={styles.caret}></div>
-      <ul className={`${styles.options} ${styles.show}`}>
+      <ul className={`${styles.options} ${isOpen ? styles.show : ""}`}>
         {options.map((option, index) => (
-          <li key={index} className={styles.option}>
+          <li
+            onClick={(e) => {
+              e.stopPropagation();
+              selectOption(option);
+              setIsOpen(false);
+            }}
+            onMouseEnter={() => setHighlightedIndex(index)}
+            key={option.value}
+            className={`${styles.option} ${
+              isOptionSelected(option) ? styles.selected : ""
+            } ${index === highlightedIndex ? styles.highlighted : ""}`}
+          >
             {option.label}
           </li>
         ))}
